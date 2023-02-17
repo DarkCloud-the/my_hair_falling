3758df9c3354cb7d786c2f8863b7028a3e32f445
The dark cloud <gusmitf@gmail.com>
2023-02-17 00:13:24 +0000

    this should fix all bugs

---
+++ my hair almost died/hair.py
@@ -0,0 +1,10 @@
+class main; 
+if getting = True:
+  return 'good' 
+    else getting = False: 
+      return 'Really bad' 
+      
+if real == False:
+  return 'not ok?'
+elif real == True:
+  return 'really bad'
