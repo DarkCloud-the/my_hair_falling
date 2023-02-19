a5ce96058a3674e04987ed9353f3e0be1edc76ed
The dark cloud <gusmitf@gmail.com>
2023-02-19 15:04:31 -0800

    ok

--- my hair almost died/green tatto.py
+++ my hair almost died/green tatto.py
@@ -1,4 +1,13 @@
-plam = boolean
-main 
-if plam = boolean
-print(can not expression)
+can_not_expression = input(print(what color do you like))
+if can_not_expression = string 
+  print("that is a valid respose")
+else:
+  print("please input somthing else")
+class bunny(sup): 
+
+  if plam = boolean:
+    print('can not expression')
+    
+  elif plam = set ValueError(105):
+    retun(True)
+    
