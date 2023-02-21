85bdf5a6951baee707440aaf09a39dc597cdaf06
The dark cloud <gusmitf@gmail.com>
2023-02-21 10:52:44 -0800

    Update just some sauce.py

--- the other things/just some sauce.py
+++ the other things/just some sauce.py
@@ -1,7 +1,11 @@
-class super(main, self).real:
+real = TimeoutError
+main = input(print("so yeah"))
+
+class super(main, self):real
   
-   creative = input("jack is dead what will do now")
+creative = input("jack is dead what will do now")
   
-  if creative == intger :
-    return ('True')
-  else:
+if creative == intger :
+  print("True")
+else:
+  print("Help wanted")
