9fe8070848027c9d870148cb5d205fe7f5e95cd7
The dark cloud <gusmitf@gmail.com>
2023-02-21 18:14:44 -0800

    Update just some sauce.py

--- the other things/just some sauce.py
+++ the other things/just some sauce.py
@@ -1,11 +1,12 @@
 real = TimeoutError
-main = input(print("so yeah"))
-
+main = input(print("so yeah "))
+self = input(print("so how many do you want"))
+numeric = (1-10)
 class super(main, self):real
   
 creative = input("jack is dead what will do now")
   
-if creative == intger :
+if creative == numeric :
   print("True")
 else:
   print("Help wanted")
