9c12cee8b404af29d11cb17d0ad86ec050cf8ed9
The dark cloud <gusmitf@gmail.com>
2023-02-26 20:41:39 -0800

    Update over hair headset.py

---
+++ my hair almost died/over hair headset.py
@@ -0,0 +1,6 @@
+class monkey(console.groupEnd(hppy)):
+  what_emotion = input("emotion are you feeling")
+  if what_emotion == [happy,sad,lonyley,]:
+  print("that work's")
+  else:
+    print("no work")
