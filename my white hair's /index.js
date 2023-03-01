f0eefa9d60f38a995bb7fee284af6042c34d4612
The dark cloud <gusmitf@gmail.com>
2023-02-28 21:37:19 -0800

    this is the fix
    and the updated the thing snd how to fix all of the changes and how too make thing fully function and how too change the interval complteley

--- my white hair's /index.js
+++ my white hair's /index.js
@@ -1,6 +1,19 @@
-const axious = require('axios');
+const axios = require('axios');
 const cheerio = require('cheerio');
+const query = 'hentai images ';
 
-const query = 'henappy images';
+axios.get('https://www.google.com/search?q-${query}Stbm-isch')
+    .then (response => {
+        const $ = cheerio. load (response.data);
+        const images = $('div.rg_meta').map((i, el) => {
+        // Get the image URL and link
+            const imageUrl = JSON.parse($(el).text( )).ou; 
+            const linkUrl = JSON.parse($(el).text( )). ru;
+            return { imageUrl, linkurl };
+        }).get();
 
-axious.get(' ')
+        // PrintIout the results in a table
+        console, log(' Image\tLink');
+        images, forEach( image => 
+    console, Log(' $(image, imageUrl)\t$(image.LinkUrl)'));
+});
