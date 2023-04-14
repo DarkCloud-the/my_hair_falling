d56366341d78df0a3faed3d17ac89c46e750677c
The dark cloud <gusmitf@gmail.com>
2023-04-13 21:27:45 -0700

    the good god

---
+++ while open == True/true but.py
@@ -0,0 +1,21 @@
+while import React from 'react'
+import { View, Text } from 'react-native'
+
+interface Props {
+  
+}
+
+const FileName = (props: Props) => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  )
+}
+
+export default FileName
+
+locals()
+const styles = StyleSheet.create({
+  style
+})
