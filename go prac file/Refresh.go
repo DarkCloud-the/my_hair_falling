25aba9b951b282f87d677a31b65dda6cc142bdbd
The dark cloud <gusmitf@gmail.com>
2023-05-17 17:31:35 -0700

    for the system function

---
+++ go prac file/Refresh.go
@@ -0,0 +1,10 @@
+import moduleName from 'module'
+setTimeout(() => {
+  
+}, delayInms)
+import PropTypes from 'prop-types'
+const contextName = React.createContext()
+state = {
+  
+}
+const contextName = React.createContext()
---
+++ go prac file/run.go
@@ -0,0 +1,23 @@
+when import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+interface Props {
+  
+}
+
+const FileName = (props: Props) => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  )
+}
+
+export default FileName
+
+const styles = StyleSheet.create({})
+export default 
+len var
+(params) => {
+  
+}
